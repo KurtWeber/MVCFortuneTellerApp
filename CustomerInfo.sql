f6c13eb915166f2318946c06f004d27b48b2c428
KurtWeber <weberkrt@msn.com>
2016-10-13 14:48:48 -0400

    Added table SQL file

---
+++ CustomerInfo.sql
@@ -0,0 +1,26 @@
+USE [FortuneTellerMVC]
+GO
+
+/****** Object:  Table [dbo].[Customers]    Script Date: 10/13/2016 2:47:26 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Customers](
+	[CustomerID] [int] IDENTITY(1,1) NOT NULL,
+	[FirstName] [nvarchar](50) NOT NULL,
+	[LastName] [nvarchar](50) NOT NULL,
+	[Age] [int] NOT NULL,
+	[BirthMonth] [int] NOT NULL,
+	[FavoriteColor] [nvarchar](50) NOT NULL,
+	[NumberOfSiblings] [nvarchar](50) NOT NULL,
+ CONSTRAINT [PK_Customers] PRIMARY KEY CLUSTERED 
+(
+	[CustomerID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
